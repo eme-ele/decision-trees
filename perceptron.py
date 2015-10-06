07e7258cdb94d3cc97ef38df801fa65e8c6a0c71
Maria Leonor Pacheco gonz?lez <pachecog@data.cs.purdue.edu>
2015-10-06 14:45:39 -0400

    impl hal daume avg percep + learn rate

--- perceptron.py
+++ perceptron.py
@@ -6,34 +6,52 @@ class perceptron(object):
         self.max_iter = max_iter
         self.learn_rate = learn_rate
         self.weights = np.array([])
-        self.weight_0 = 0 #bias
+        self.bias = 0 #bias
+        self.avg_weights = np.array([])
+        self.avg_bias = np.array([])
 
     # weights should be set to random small values
     def init_weights(self, num_feats):
-        self.weight_0 = 0
+        self.bias = 0
         self.weights = np.zeros(num_feats)
+        self.avg_weights = np.zeros(num_feats)
+        self.avg_bias = 0
 
     def sgn(self, sample):
         dot_product = 0
         #print sample
         for (j,v) in sample:
             dot_product += v * self.weights[j]
-        dot_product += 1*self.weight_0
+        dot_product += self.bias
         if dot_product > 0:
             return 1
         else:
             return -1
 
-    def update_weights(self, x, y):
-        # calculate bias weight_0
-        self.weight_0 = self.weight_0 + self.learn_rate * y * 1
+    def avg_sgn(self, sample):
+        dot_product = 0
+        for (j, v) in sample:
+            dot_product += v * (self.weights[j] - self.avg_weights[i]/c)
+        dot_product += self.bias - self.avg_bias/c
+        dot_product > 0:
+            return 1
+        else:
+            return -1
+        
+    def update_weights(self, x, y, c):
+        # calculate bias 
+        b = self.learn_rate * y
+        self.bias += b
+        self.avg_bias += b * c
         for (j,v) in x:
-            self.weights[j] += self.learn_rate * y * v
-
+            w = self.learn_rate * y * v
+            self.weights[j] += w
+            self.avg_weights += w * c
 
     def fit(self, samples, labels, n_feats):
         # init weights for all features
         self.init_weights(n_feats)
+        c = 1.0
 
         for i in xrange(self.max_iter):
             num_mistakes = 0
@@ -44,6 +62,7 @@ class perceptron(object):
                 if y_pred != labels[j]:
                     num_mistakes += 1
                     self.update_weights(samples[j], labels[j])
+                c += 1
 
             print i, " - # mistakes", num_mistakes
 
@@ -51,5 +70,8 @@ class perceptron(object):
                 return
         return
 
-    def predict(self, samples):
-        return [self.sgn(s) for s in samples]
+    def predict(self, samples, avg=1):
+        if avg:
+            return [self.avg_sgn for s in samples]
+        else:
+            return [self.sgn(s) for s in samples]
