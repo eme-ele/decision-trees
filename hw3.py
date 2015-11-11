c72130e15f8c0c77a3473ab43ef0934e470703d7
ML <emele18@gmail.com>
2015-11-10 19:50:33 -0500

    gradient running

--- hw3.py
+++ hw3.py
@@ -1,4 +1,10 @@
 import optparse
+import csv
+
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+
+from feature_extractor import ngrams_fe
+from subgradient_descent import subgradient_descent
 
 def parse_and_validate():
     ## parse
@@ -29,9 +35,28 @@ def parse_and_validate():
     return opts
 
 ## Gradient Descent Algorithm
-def GD(max_iterations, regularization, step_size, lmbd, feature_set):
+def GD(max_iterations, regularization, step_size, lmbd, feature_set,
+       num_feats, samples, labels):
+    sgd = subgradient_descent(max_iterations, regularization, step_size, lmbd)
+    sgd.fit(samples, labels, num_feats)
+    return sgd
 
-    return
+## parse files
+def parse_file(filename):
+    samples = []
+    labels = []
+    with open(filename, 'rb') as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            samples.append(row[0])
+            labels.append(1 if row[1] == '+' else -1)
+    return samples, labels
+
+## preprocessing
+def preprocess(samples):
+    samples = [s.lower() for s in samples]
+    #samples = [s.split() for s in samples]
+    return samples
 
 # main
 # ----
@@ -40,11 +65,47 @@ def GD(max_iterations, regularization, step_size, lmbd, feature_set):
 
 def main():
     opts = parse_and_validate()
-    print opts.max_iterations, opts.regularization, opts.step_size, opts.lmbd, opts.feature_set
+    #print opts.max_iterations, opts.regularization, opts.step_size, opts.lmbd, opts.feature_set
 
     # ====================================
     # WRITE CODE FOR YOUR EXPERIMENTS HERE
     # ====================================
+
+    train_samples, train_labels = parse_file("data/hw3/train.csv")
+    test_samples, test_labels = parse_file("data/hw3/test.csv")
+
+    print "num train_samples", len(train_labels),
+    print "\tpos:", train_labels.count(1), "neg:", train_labels.count(-1)
+    print "num test_samples", len(test_labels)
+    print "\tpos:", test_labels.count(1), "neg:", test_labels.count(-1)
+
+    print "preprocessing..."
+    train_samples = preprocess(train_samples)
+    test_samples = preprocess(test_samples)
+
+    if opts.feature_set == 1:
+        fe = ngrams_fe(1)
+    elif opts.feature_set == 2:
+        fe = ngrams_fe(2)
+    elif opts.feature_set == 3:
+        fe = ngrams_fe(1,2)
+
+    print "training feature extractor..."
+    fe.train(train_samples, train_labels)
+    print "extracting features for complete data (train/test)..."
+    train_features = fe.extract(train_samples)
+    test_features = fe.extract(test_samples)
+
+    print "training classifier..."
+    classifier = GD(opts.max_iterations, opts.regularization, opts.step_size,
+                    opts.lmbd, opts.feature_set, fe.n_feats, train_features, train_labels)
+    results = classifier.predict(test_features)
+
+    print classification_report(test_labels, results)
+    print accuracy_score(test_labels, results)
+    print confusion_matrix(test_labels, results)
+
+
 
 if __name__ == '__main__':
     main()
--- subgradient_descent.py
+++ subgradient_descent.py
@@ -1,66 +1,78 @@
 import random
+import numpy as np
 
 class subgradient_descent(object):
 
-    def __init__(self, max_iter, num_feats, lmbd, regularization, step_size):
+    def __init__(self, max_iter, regularization, step_size, lmbd):
         # parameters
         self.max_iter = max_iter
-        self.lmbd = lmbd
         self.regularization = regularization
         self.step_size = step_size
-        self.num_feats = num_feats
-        # weights and gradients
-        self.weights = np.zeros(num_feats)
-        self.bias = 0.0
-        self.gradient_w = np.zeros(num_feats)
-        self.gradient_b = 0.0
+        self.lmbd = lmbd
 
     # condition for hinge_loss subgradient: y (w * x + b)
     # if y (w * x + b) > 1, subgradient will be 0
     # in any other case, should calculate
-    def hinge_loss_sg_condition(x, y):
-        # dot product between x and weights
+    def hinge_loss_term(self, x, y):
         dot_product = 0.0
         for (j, v) in x:
             dot_product += v * self.weights[j]
         dot_product += self.bias
-
         return dot_product*y
+
+    def hinge_loss(self, x, y):
+        term = self.hinge_loss_term(x, y)
+        return max(0, 1 - term)
+
+    def avg_loss(self, samples, labels):
+        loss = 0.0
+        for (x,y) in zip(samples, labels):
+            loss += self.hinge_loss(x,y)
+        return loss/len(labels)
 
     # update gradients with the subgradient
     # it is called only when subgradient != 0
-    def update_gradients(x, y):
-        sub_gradient = np.zeros(len())
+    def update_gradients(self, x, y):
         for (j, v) in x:
             sub_gradient_j = y * v
             self.gradient_w[j] += sub_gradient_j
         self.gradient_b += y
 
-    def l2_update():
+    def l2_update(self):
         for i in xrange(0, self.num_feats):
             self.gradient_w[i] -= self.lmbd * self.weights[i]
 
-    def l1_update():
+    def l1_update(self):
         for i in xrange(0, self.num_feats):
             self.gradient_w[i] -= self.lmbd
 
-    def update_weights():
+    def update_weights(self):
         for i in xrange(0, self.num_feats):
             self.weights[i] += self.step_size * self.gradient_w[i]
-        self.bias += self.step * self.gradient_b
+        self.bias += self.step_size * self.gradient_b
 
-    def fit(self, samples, labels):
-        num_samples = samples.shape[0]
+    def fit(self, samples, labels, num_feats):
+        num_samples = len(labels)
+        #print "num_samples", num_samples
+        #print labels
+        #exit()
+        self.num_feats = num_feats
+
+        # weights
+        self.weights = np.zeros(num_feats)
+        self.bias = 0.0
 
         for i in xrange(self.max_iter):
+            # gradients
             self.gradient_w = np.zeros(self.num_feats)
             self.gradient_b = 0.0
 
             for d in xrange(num_samples):
-                sg_condition = self.hinge_loss_sg_condition(samples[d], labels[d])
+                x_d = samples[d]
+                y_d = labels[d]
                 # if subgradient is not 0
-                if sg_condition <= 1:
-                    self.update_gradients()
+                if self.hinge_loss_term(x_d, y_d) <= 1:
+                    self.update_gradients(x_d, y_d)
 
             # add in regularization term
             if self.regularization == 'l2':
@@ -70,16 +82,17 @@ class subgradient_descent(object):
 
             # updates both weights and bias
             self.update_weights()
+            print "Iter", i, "Avg Loss", self.avg_loss(samples, labels)
 
     def predict_one(self, sample):
         dot_product = 0.0
         for (j, v) in sample:
             dot_product += v * self.weights[j]
-        dot+product += self.bias
-        if dot_product > 0:
-            return 1
-        else:
+        dot_product += self.bias
+        if dot_product < 0:
             return -1
+        else:
+            return 1
 
     def predict(self, samples):
         return [self.predict_one(s) for s in samples]
